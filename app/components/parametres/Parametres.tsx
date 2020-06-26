ed65fd2f3dbdb906d5b91d60a6e770bf8066526e
Hoyame <zou.hoyame@gmail.com>
2020-06-26 15:19:32 +0200

    feat: update userinfo component

--- app/components/parametres/Parametres.tsx
+++ app/components/parametres/Parametres.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './Parametres.scss';
 import ServerInfo from '../utils/ServerInfo/ServerInfo'
 import UserInfo from '../utils/UserInfo/UserInfo';
+import PageInfo from '../utils/PageInfo/PageInfo';
 
 const ParametresPage = () => {
   return (
     <React.Fragment>
       <ServerInfo />
       <UserInfo />
+      <PageInfo titlePage='Parametres' />
 
 
     </React.Fragment>
