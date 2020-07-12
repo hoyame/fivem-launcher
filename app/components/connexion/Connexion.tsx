7207ee8bd3d0e1c7aba019ada1fe074b6d55db7f
Hoyame <zou.hoyame@gmail.com>
2020-07-13 00:34:08 +0200

    feat: add connexion page

--- app/components/connexion/Connexion.tsx
+++ app/components/connexion/Connexion.tsx
@@ -6,22 +6,33 @@ import PageInfo from '../utils/PageInfo/PageInfo';
 import styles from './Connexion.scss';
 
 const ConnexionPage = () => {
-  const [state, setstate] = useState({
-    name: 'zboub',
-    osef: 'zbi'
+  const [state, setState] = useState({
+    name: 'Pseudo',
+    discord: 'Discord',
+    img: 'Lien de la photo de profil'
   })
 
-  const saveInformations = () => {
-
-  }
+  const pp = "https://cdn.discordapp.com/avatars/643225867929124874/03d4be07bdddc7b9970f85a3d483b736.png?size=256"
 
   const returnComponent = () => {
     return (
-      <div className={styles['connexion-component']}>
-        <div className={styles['connexion-component-container']}></div>
-        <div className={styles['connexion-component-container']}></div>
-        <div className={styles['connexion-component-container']}></div>
-      </div>
+      <React.Fragment>
+        <div className={styles['connexion-component']}>
+          <p className={styles['connexion-title']}>Informations</p>
+          <div className={styles['connexion-component-container']}>
+            <input className="connexion-component-input" type="text" value={state.name} onChange={e => setState({ ...state, name: e.target.value })} />
+          </div>
+          <div className={styles['connexion-component-container']}>
+            <input className="connexion-component-input" type="text" value={state.discord} onChange={e => setState({ ...state, discord: e.target.value })} />
+          </div>
+          <div className={styles['connexion-component-container']}>
+            <input type="text" value={state.img} onChange={e => setState({ ...state, img: e.target.value })} />
+          </div>
+        </div>
+
+        <img className={styles['connexion-photo-container']} src={pp}></img>
+      </React.Fragment>
+
     );
   }
 
