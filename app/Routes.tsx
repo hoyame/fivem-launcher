74498d9affeff51893c387302e3fd4b86b576834
Hoyame <zou.hoyame@gmail.com>
2020-07-12 23:41:20 +0200

    feat: update routes

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -8,6 +8,7 @@ import InformationsPage from './components/informations/Informations';
 import AidePage from './components/aide/Aide';
 import SettingsPage from './components/parametres/Parametres';
 import ContenuPage from './components/contenu/Contenu';
+import ConnexionPage from './components/connexion/connexion';
 
 import './app.global.css';
 
@@ -21,6 +22,7 @@ export default function Routes() {
           <Route path={routes.AIDE} exact component={AidePage} />
           <Route path={routes.SETTINGS} exact component={SettingsPage} />
           <Route path={routes.CONTENU} exact component={ContenuPage} />
+          <Route path={routes.CONNEXION} exact component={ConnexionPage} />
         </Switch>
       </App>
     </div>
