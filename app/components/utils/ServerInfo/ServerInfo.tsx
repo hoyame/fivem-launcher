35bd02f86b568c286132f393dee77b1413a796f1
Hoyame <zou.hoyame@gmail.com>
2020-07-16 21:55:50 +0200

    update: name server

--- app/components/utils/ServerInfo/ServerInfo.tsx
+++ app/components/utils/ServerInfo/ServerInfo.tsx
@@ -7,7 +7,7 @@ const ServerInfo = () => {
     <React.Fragment>
       <div className={styles['server-info']}>
         <img className={styles['server-info-img']} src="https://cdn.discordapp.com/icons/329623622819905546/aa0c19ef3bc3607262c11b69bad3a572.webp?size=128"></img>
-        <p className={styles['server-info-name']}>GTALife</p>
+        <p className={styles['server-info-name']}>VMLife</p>
       </div>
     </React.Fragment>
  );
