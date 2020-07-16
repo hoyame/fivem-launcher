f7029d03ca4027a28b58f2d960e272c4934cb5a4
Hoyame <zou.hoyame@gmail.com>
2020-07-16 21:52:48 +0200

    connexion: fix image preview

--- app/components/connexion/Connexion.tsx
+++ app/components/connexion/Connexion.tsx
@@ -8,29 +8,28 @@ import { ButtonProps } from '../informations/Informations';
 
 const ConnexionPage = () => {
   const [state, setState] = useState({
-    name: 'Pseudo',
-    discord: 'Discord',
-    img: 'Lien de la photo de profil'
+    name: localStorage.getItem('aPseudo') || 'Pseudo',
+    discord: localStorage.getItem('aDiscord') || 'Discord',
+    img: localStorage.getItem('aImg') || 'Lien de la photo de profil'
   })
 
   const saveData = () => {
-    localStorage.removeItem('cPseudo');
-    localStorage.removeItem('cDiscord');
-    localStorage.removeItem('cImg');
+    localStorage.removeItem('aPseudo');
+    localStorage.removeItem('aDiscord');
+    localStorage.removeItem('aImg');
 
-    localStorage.setItem('cPseudo', state.name);
-    localStorage.setItem('cDiscord', state.discord);
-    localStorage.setItem('cImg', state.img);
+    localStorage.setItem('aPseudo', state.name);
+    localStorage.setItem('aDiscord', state.discord);
+    localStorage.setItem('aImg', state.img);
   }
 
   let pp: string;
 
   if (state.img == 'Lien de la photo de profil') {
     pp = "https://cdn.discordapp.com/avatars/643225867929124874/03d4be07bdddc7b9970f85a3d483b736.png?size=256"
+  } else {
+    pp = state.img
   }
-
-  const name = localStorage.getItem('cPseudo') || state.name;
-  const discord = localStorage.getItem('cDiscord') || state.discord;
 
   const returnComponent = () => {
     return (
@@ -38,10 +37,10 @@ const ConnexionPage = () => {
         <div className={styles['connexion-component']}>
           <p className={styles['connexion-title']}>Informations</p>
           <div className={styles['connexion-component-container']}>
-            <input className="connexion-component-input" type="text" value={name} onChange={e => setState({ ...state, name: e.target.value })} />
+            <input className="connexion-component-input" type="text" value={state.name} onChange={e => setState({ ...state, name: e.target.value })} />
           </div>
           <div className={styles['connexion-component-container']}>
-            <input className="connexion-component-input" type="text" value={discord} onChange={e => setState({ ...state, discord: e.target.value })} />
+            <input className="connexion-component-input" type="text" value={state.discord} onChange={e => setState({ ...state, discord: e.target.value })} />
           </div>
           <div className={styles['connexion-component-container']}>
             <input type="text" value={state.img} onChange={e => setState({ ...state, img: e.target.value })} />
